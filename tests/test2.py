e3db6eaae571c4be05c058fcc68eec9a7bdc4f5c
Julien Lengrand-Lambert <julien@lengrand.fr>
2013-01-11 16:06:57 +0100

    Creates test folder.
    Not to perform unit tests (I really need to put all that stuff in practise some day), but some silly tests about the best way to chieve next objective.
    
    Next:
    - Add a hashtag table in database, to keep track of all current trendy hashtags. That way it can easily be used by everyone and we can get some more tracability.

---
+++ tests/test2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from time import sleep
+import test3
+# this is counter, it never has to cha nge list of hashtags
+
+class counter:
+    def __init__(self):
+
+        while True:
+            #print self.in_data
+            sleep(1)
+            print test3.data
