e81a5487cd8cb9357fc343702fb31d7dfa50f6f5
Julien Lengrand-Lambert <julien@lengrand.fr>
2013-01-14 22:42:42 +0100

    Changes LeaderBoard class pattern. Leaderboard is now the root class of all other printers. A Printer will be dedicated to a type of output media (file dump, console, html, ....)

--- twiderboard/leader.py
+++ twiderboard/leader.py
@@ -23,27 +23,25 @@ import signal
 
 
 class LeaderBoard():
-
-    def __init__(self, hashtag, size=10):
+    def __init__(self, hashtag=None, size=10):
         self.url = engine_url
         self.hashtag = hashtag
         self.size = size
 
-        self.interval = 2   # Number of seconds between each leaderboard display
-
-        self.leader_unit = RepeatingTimer(self.interval, self.leaderprint)
+        self.interval = 10
+        self.leader_proc = RepeatingTimer(self.interval, self.leader_print)
 
     def start(self):
         """
         Starts counting new members periodically
         """
-        self.leader_unit.start()
+        self.leader_proc.start()
 
     def stop(self):
         """
         Stops counting new members periodically
         """
-        self.leader_unit.stop()
+        self.leader_proc.stop()
 
     def connect(self):
         """
@@ -59,7 +57,49 @@ class LeaderBoard():
 
         return Session()  # Bridges class to db
 
-    def leaderprint(self):
+    def get_hashtags():
+        """
+        Returns a list of all current tracked hashtags\
+        TODO: Implement
+        """
+
+    def get_leaders(self):
+        """
+        Returns a list of the current leaders for the chosen hashtags.
+        self.hashtag can be either the hashtag that has to be printed or None.
+        If self.hashtag is None, the leaders for all current tracked hashtags will be outputed.
+
+
+        The result will be a list of twitter usernames, with the current number
+        of tweets containing the hashtag they sent.
+        The list is of max size size, but can be smaller of even empty if no
+        user has been detected yet.
+
+        The result is of type :
+        [[#hashtag1, [Leader1, Leader2, ...]],  [#hashtag2, [Leader1, Leader2, ...]]]
+        #FIXME : Ugly and done in the train. Get back to this soon .
+        """
+        session = self.connect()
+        leaders = []
+        if self.hashtag is None:
+            hashtags = self.get_hashtags()
+            #TODO: Fill this
+        else:
+            l_query = session.query(Member).filter(Member.hashtag == self.hashtag).order_by(desc(Member.count))
+            top_members = l_query.all()
+            if self.size > 0:
+                top_members = top_members[0:self.size]
+            leaders.append([self.hashtag, top_members])
+
+        return leaders
+
+
+class DumbLeaderBoard(LeaderBoard):
+
+    def __init__(self, hashtag, size=10):
+        LeaderBoard.__init__(self, hashtag, size)
+
+    def leader_print(self):
         """
         Periodically retrieves the leaders for the given hashtag
         and prints them out
@@ -67,29 +107,34 @@ class LeaderBoard():
         leaders = self.get_leaders()
         self.print_leaders(leaders)
 
-    def get_leaders(self):
-        """
-        Returns the current leaders of the competition for the given hashtag
-        The result will be a list of twitter usernames, with the current number
-        of tweets containing the hashtag they sent.
-        The list is of max size size, but can be smaller of even empty if no
-        user has been detected yet.
-        """
-        session = self.connect()
-        l_query = session.query(Member).filter(Member.hashtag == self.hashtag).order_by(desc(Member.count))
-        leaders = l_query.all()
-        if self.size > 0:
-            leaders = leaders[0:self.size]
+    # def get_leaders(self):
+    #     """
+    #     Returns the current leaders of the competition for the given hashtag
+    #     The result will be a list of twitter usernames, with the current number
+    #     of tweets containing the hashtag they sent.
+    #     The list is of max size size, but can be smaller of even empty if no
+    #     user has been detected yet.
+    #     """
+    #     session = self.connect()
+    #     #l_query = session.query(Member).filter(Member.hashtag == self.hashtag).order_by(desc(Member.count))
+    #     print self.hashtag
+    #     l_query = session.query(Member).order_by(desc(Member.count))
+    #     leaders = l_query.all()
+    #     if self.size > 0:
+    #         leaders = leaders[0:self.size]
 
-        return leaders
+    #     return leaders
 
     def print_leaders(self, leaders):
         """
         Dumps the list of leaders on the console
+        leaders is of type :
+        [[#hashtag1, [Leader1, Leader2, ...]],  [#hashtag2, [Leader1, Leader2, ...]]]
         """
-        print "#########"
-        for leader in leaders:
-            print "%s - %d" % (leader.author, leader.count)
+        for hashtag, members in leaders:
+            print "######### %s #########" % (hashtag)
+            for member in members:
+                print "%s - %d" % (member.author, member.count)
 
 # ---------
 def stop_handler(signal, frame):
@@ -105,6 +150,6 @@ if __name__ == '__main__':
     signal.signal(signal.SIGINT, stop_handler)
 
     # Initiates LeaderBoard and starts it
-    l = LeaderBoard("#nowplaying", 10)
+    l = DumbLeaderBoard("#nowplaying", 10)
     print "Press CTRL + C to stop application"
     l.start()
