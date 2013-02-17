2e8a425cb7efcb01fcd654a986f8de0ec716b899
Julien Lengrand-Lambert <julien@lengrand.fr>
2013-02-17 18:15:48 +0100

    Structures the html templates a bit more and adds categories in the page.
    NExt steps:
    - List all currently tracked hashtags on main page
    - Add icon to remove those hashtags next to them.

--- twiderboard/flask_app.py
+++ twiderboard/flask_app.py
@@ -75,7 +75,17 @@ def stop():
 
 @app.route('/')
 def index():
+    return render_template('index.html')
+
+
+@app.route('/statistics')
+def statistics():
     return render_template('statistics.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
